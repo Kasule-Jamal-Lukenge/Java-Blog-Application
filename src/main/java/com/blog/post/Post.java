eb09559df83ac4a8cff3a83588bf00bf2f816658
JAMAL-111 <jamalkasule123@gmail.com>
2025-08-20 12:27:41 -0500

    First Commit On This Project With The User and Post Classes Created

---
+++ src/main/java/com/blog/post/Post.java
@@ -0,0 +1,32 @@
+package com.blog.post;
+
+import com.blog.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length=180)
+    private String title;
+
+    @Column(nullable = false, columnDefinition="TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="author_id", nullable=false)
+    private User author;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+}
